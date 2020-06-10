06b0a96b891fc4a9743964e0f35f315b881b6c3a
andronikmk <andronikmk@gmail.com>
2020-06-10 16:41:46 -0400

    practice session 2

--- queue/queue.py
+++ queue/queue.py
@@ -41,6 +41,29 @@ Stretch: What if you could only use instances of your Stack class to implement t
 
 
 
+# import sys
+# sys.path.append('../singly_linked_list')
+# from singly_linked_list import LinkedList
+
+# class Queue:
+#    def __init__(self):
+#       self.size = 0
+#       self.storage = LinkedList()
+
+#    def __len__(self):
+#       return self.size
+
+#    def enqueue(self, value):
+#       self.storage.add_to_tail(value)
+#       self.size += 1
+
+#    def dequeue(self):
+#       if self.size >= 1:
+#          value = self.storage.remove_head()
+#          self.size -= 1
+#          return value
+
+
 import sys
 sys.path.append('../singly_linked_list')
 from singly_linked_list import LinkedList
